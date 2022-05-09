2a4290f78147ec5d1e08d647e899884e7a11ce28
William Holberg <holbergw@gmail.com>
2022-05-09 16:02:33 +0200

    trying to fix bug

--- SEEP/src/main/projectManagementSystem/ProjectManagementSystem.java
+++ SEEP/src/main/projectManagementSystem/ProjectManagementSystem.java
@@ -2,7 +2,6 @@ package projectManagementSystem;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Scanner;
 
 public class ProjectManagementSystem {
     private static ArrayList<Project> projects = new ArrayList<Project>();
